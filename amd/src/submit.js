6d7072e483d50d05b2e87580180e63b42ab18077
Mark Sharp <mark.sharp@solent.ac.uk>
2022-10-07 11:30:09 +0100

    JS linting and AMD rebuild

--- amd/src/submit.js
+++ amd/src/submit.js
@@ -12,9 +12,9 @@ define(['jquery', 'core/str', 'core/notification', 'report_ee/submit'], function
 
     return {
         init: function(admin) {
-          $("input[name='locked']").click(function(){
+          $("input[name='locked']").click(function() {
             var lockedValue = $("input[name='locked']:checked").val();
-            if(lockedValue == 1){
+            if (lockedValue == 1) {
               var lockedWarning = str.get_strings([
                   {key: 'lockedwarning', component: 'report_ee'},
               ]);
@@ -22,11 +22,11 @@ define(['jquery', 'core/str', 'core/notification', 'report_ee/submit'], function
                    $('.lockedwarning').text(M.util.get_string('lockedwarning', 'report_ee'));
                    $('.lockedwarning').addClass("alert alert-danger");
               }).fail(notification.exception);
-            }else{
+            } else {
               $('.lockedwarning').text("");
               $(".lockedwarning").removeClass("alert alert-danger");
-              if(admin == 1){
-                $( "#id_locked" ).prop( "disabled", true );
+              if (admin == 1) {
+                $("#id_locked").prop("disabled", true);
               }
             }
           });
